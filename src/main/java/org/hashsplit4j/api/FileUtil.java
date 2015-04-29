789c60a8f3237b4839633250e632cf933d061bbc
dylan <dylan@dylan-G75VW>
2015-04-30 00:11:48 +1200

    Init commit for FileSystemHashStore

---
+++ src/main/java/org/hashsplit4j/api/FileUtil.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.hashsplit4j.api;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+/**
+ *
+ * @author dylan
+ */
+public class FileUtil {
+
+    public static final String DEFAULT_CHARSET = "UTF-8";
+
+    public static String readFile(File file) throws IOException {
+        byte[] data = org.apache.commons.io.FileUtils.readFileToByteArray(file);
+        return new String(data);
+    }
+
+    public static String readFile(String path, Charset encoding) throws IOException {
+        byte[] encoded = Files.readAllBytes(Paths.get(path));
+        return new String(encoded, encoding);
+    }
+
+    public static String readOrCreateFile(File file, Boolean autoCreate) throws IOException {
+        if (file.isFile()) {
+            if (!file.exists()) {
+                file.createNewFile();
+                return null;
+            }
+            return readFile(file);
+        }
+        return null;
+    }
+
+    public static void writeFile(File file, byte[] data, boolean append) throws IOException {
+        org.apache.commons.io.FileUtils.writeByteArrayToFile(file, data, append);
+    }
+
+    public static void writeFile(File file, byte[] data, boolean append, Boolean autoCreate) throws IOException {
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+        writeFile(file, data, append);
+    }
+}
---
+++ src/main/java/org/hashsplit4j/store/FileSystemHashStore.java
@@ -0,0 +1,123 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.hashsplit4j.store;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import org.hashsplit4j.api.Fanout;
+import org.hashsplit4j.api.FanoutImpl;
+import org.hashsplit4j.api.FileUtil;
+import org.hashsplit4j.api.HashStore;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author dylan
+ */
+public class FileSystemHashStore implements HashStore {
+
+    private static final Logger log = LoggerFactory.getLogger(FileSystemHashStore.class);
+
+    private final File envHome;
+    private final String FILE_EXT = ".hash";
+
+    public FileSystemHashStore(File envHome) {
+        this.envHome = envHome;
+    }
+
+    @Override
+    public void setChunkFanout(String hash, List<String> blobHashes, long actualContentLength) {
+        File chunkFanout = new File(envHome, "chunks/" + hash + FILE_EXT);
+        String hashes = formatFanout(blobHashes, actualContentLength);
+        try {
+            FileUtil.writeFile(chunkFanout, hashes.getBytes(), false, Boolean.TRUE);
+        } catch (IOException ex) {
+            log.info("Unable to save chunk fanout to file", ex);
+        }
+    }
+
+    @Override
+    public void setFileFanout(String hash, List<String> fanoutHashes, long actualContentLength) {
+        File fileFanout = new File(envHome, "files/" + hash + FILE_EXT);
+        String hashes = formatFanout(fanoutHashes, actualContentLength);
+        try {
+            FileUtil.writeFile(fileFanout, hashes.getBytes(), false, Boolean.TRUE);
+        } catch (IOException ex) {
+            log.info("Unable to save file fanout to file", ex);
+        }
+    }
+
+    @Override
+    public Fanout getFileFanout(String fileHash) {
+        File fileFanout = new File(envHome, "files/" + fileHash + FILE_EXT);
+        if(fileFanout.exists()){
+            try {
+                String hashes = FileUtil.readFile(fileFanout);
+                return parseFanout(hashes);
+            } catch (IOException ex) {
+                log.info("Unable to read file fanout to file", ex);
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public Fanout getChunkFanout(String fanoutHash) {
+        File chunkFanout = new File(envHome, "chunks/" + fanoutHash + FILE_EXT);
+        if(chunkFanout.exists()){
+            try {
+                String hashes = FileUtil.readFile(chunkFanout);
+                return parseFanout(hashes);
+            } catch (IOException ex) {
+                log.info("Unable to read chunk fanout to file", ex);
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public boolean hasChunk(String fanoutHash) {
+        File chunkFanout = new File(envHome, "chunks/" + fanoutHash + FILE_EXT);
+        return chunkFanout.exists();
+    }
+
+    @Override
+    public boolean hasFile(String fileHash) {
+        File fileFanout = new File(envHome, "files/" + fileHash + FILE_EXT);
+        return fileFanout.exists();
+    }
+
+    private String formatFanout(List<String> blobHashes, long actualContentLength) {
+        StringBuilder sb = new StringBuilder();
+
+        for (String hash : blobHashes) {
+            sb.append(hash).append(",");
+        }
+
+        sb.append(actualContentLength);
+
+        return sb.toString();
+    }
+
+    private Fanout parseFanout(String fan) {
+        String[] parts = fan.split(",");
+        List<String> blobHashes = new ArrayList<>();
+        Long actualContentLength = null;
+        int len = parts.length;
+        int count = 0;
+        for (String part : parts) {
+            if (++count < len) {
+                blobHashes.add(part);
+            } else {
+                actualContentLength = Long.valueOf(part);
+            }
+        }
+        return new FanoutImpl(blobHashes, actualContentLength);
+    }
+}
