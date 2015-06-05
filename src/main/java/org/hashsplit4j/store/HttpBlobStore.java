3972309da054050611c40b2160aa12ee53793015
brad <brad@brad-little-laptop>
2015-06-06 10:53:04 +1200

    2.3.4 introduce http hash store, refactor httpblobstore to use external http transport service

--- src/main/java/org/hashsplit4j/store/HttpBlobStore.java
+++ src/main/java/org/hashsplit4j/store/HttpBlobStore.java
@@ -1,33 +1,8 @@
 package org.hashsplit4j.store;
 
 import io.milton.common.Path;
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import org.apache.commons.io.IOUtils;
 import org.hashsplit4j.api.BlobStore;
 import org.slf4j.LoggerFactory;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpHost;
-import org.apache.http.HttpResponse;
-import org.apache.http.auth.AuthScope;
-import org.apache.http.auth.UsernamePasswordCredentials;
-import org.apache.http.client.AuthCache;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.CredentialsProvider;
-import org.apache.http.client.ResponseHandler;
-import org.apache.http.client.config.RequestConfig;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPut;
-import org.apache.http.client.protocol.HttpClientContext;
-import org.apache.http.entity.ByteArrayEntity;
-import org.apache.http.impl.auth.BasicScheme;
-import org.apache.http.impl.client.BasicAuthCache;
-import org.apache.http.impl.client.BasicCredentialsProvider;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
 
 /**
  * Implements getting and setting blobs over HTTP
@@ -38,31 +13,30 @@ public class HttpBlobStore implements BlobStore {
 
     private static final org.slf4j.Logger log = LoggerFactory.getLogger(HttpBlobStore.class);
 
-    private final CredentialsProvider credsProvider;
-    private int timeout = 5000;
-    private final String server;
-    private final int port;
+    private final HashsplitHttpTransport httpTransport;
     private Path basePath;
     private long gets;
     private long sets;
 
 
-    private final HttpHost preemptiveAuthTarget;
-    private final AuthCache authCache = new BasicAuthCache();
-    private final BasicScheme basicAuth = new BasicScheme();
 
     public HttpBlobStore(String server, int port, String rootPath, String username, String password) {
-        this.server = server;
-        this.port = port;
+        httpTransport = new HashsplitHttpTransport(server, port, username, password);
+//        if( !rootPath.startsWith("/")) {
+//            throw new IllegalArgumentException("Root path must be absolute,ie start with a slash: " + rootPath);
+//        }
         this.basePath = Path.path(rootPath);
-        credsProvider = new BasicCredentialsProvider();
-        credsProvider.setCredentials(
-                new AuthScope(server, port),
-                new UsernamePasswordCredentials(username, password));
-
-        preemptiveAuthTarget = new HttpHost(server, port, "http");
-        authCache.put(preemptiveAuthTarget, basicAuth);
     }
+
+    public HttpBlobStore(HashsplitHttpTransport httpTransport, Path rootPath) {
+        this.httpTransport = httpTransport;
+//        if( rootPath.isRelative() ) {
+//            throw new IllegalArgumentException("Root path must be absolute,ie start with a slash: " + rootPath);
+//        }
+        this.basePath = rootPath;
+    }
+
+
 
     @Override
     public void setBlob(String hash, byte[] bytes) {
@@ -70,7 +44,7 @@ public class HttpBlobStore implements BlobStore {
             return;
         }
         Path destPath = basePath.child(hash + "");
-        put(destPath.toString(), bytes);        
+        httpTransport.put(destPath.toString(), bytes);
     }
 
     @Override
@@ -81,22 +55,10 @@ public class HttpBlobStore implements BlobStore {
 
     @Override
     public byte[] getBlob(String hash) {
-        Path destPath = basePath.child(hash + "");
-        return get(destPath.toString());
+        Path destPath = basePath.child(hash);
+        return httpTransport.get(destPath.toString());
     }
 
-    private byte[] getBlobSingle(String hash) throws Exception {
-        Path destPath = basePath.child(hash + "");
-        return get(destPath.toString());
-    }
-
-    public int getTimeout() {
-        return timeout;
-    }
-
-    public void setTimeout(int timeout) {
-        this.timeout = timeout;
-    }
 
     /**
      * Base url to PUT to, hash will be appended. Must end with a slash
@@ -123,89 +85,6 @@ public class HttpBlobStore implements BlobStore {
 
     @Override
     public String toString() {
-        return "HttpBlobStore: " + server + ":" + port;
-    }
-
-    
-
-    
-
-    public byte[] get(String path) {
-        HttpClientContext localContext = HttpClientContext.create();
-        localContext.setAuthCache(authCache);
-        
-        RequestConfig reqConfig = RequestConfig.custom()
-                .setSocketTimeout(timeout)
-                .setConnectTimeout(timeout)
-                .build();
-        CloseableHttpClient client = HttpClients.custom()
-                .setDefaultCredentialsProvider(credsProvider)
-                .setDefaultRequestConfig(reqConfig)
-                .build();
-        long tm = System.currentTimeMillis();
-        try {
-            URI uri = new URI("http", null, server, port, path, null, null);
-            HttpGet m = new HttpGet(uri);
-            ResponseHandler<byte[]> responseHandler = new ResponseHandler<byte[]>() {
-
-                @Override
-                public byte[] handleResponse(final HttpResponse response) throws ClientProtocolException, IOException {
-                    int status = response.getStatusLine().getStatusCode();
-                    if (status >= 200 && status < 300) {
-                        HttpEntity entity = response.getEntity();
-                        return entity != null ? EntityUtils.toByteArray(entity) : new byte[0];
-                    } else if (status == 404) {
-                        return null;
-                    } else {
-                        throw new ClientProtocolException("Unexpected response status: " + status);
-                    }
-                }
-
-            };
-            byte[] responseBody = client.execute(m, responseHandler, localContext);
-            return responseBody;
-        } catch(java.net.SocketTimeoutException ex) {
-            tm = System.currentTimeMillis() - tm;
-            throw new RuntimeException("Socket timeout: server=" + server + "; port=" + port + "Configured timeout=" + timeout + " actual time=" + tm + "ms" , ex);
-        } catch (URISyntaxException | IOException ex) {
-            throw new RuntimeException("server=" + server + "; port=" + port + "; path=" + path, ex);
-        } finally {
-            IOUtils.closeQuietly(client);
-        }
-    }
-
-    public void put(String path, byte[] bytes) {
-        HttpClientContext localContext = HttpClientContext.create();
-        localContext.setAuthCache(authCache);
-
-        RequestConfig reqConfig = RequestConfig.custom()
-                .setSocketTimeout(timeout)
-                .setConnectTimeout(timeout)
-                .build();
-        CloseableHttpClient client = HttpClients.custom()
-                .setDefaultCredentialsProvider(credsProvider)
-                .setDefaultRequestConfig(reqConfig)
-                .build();
-        CloseableHttpResponse response = null;
-        try {
-            URI uri = new URI("http", null, server, port, path, null, null);
-            HttpPut m = new HttpPut(uri);
-            HttpEntity requestEntity = new ByteArrayEntity(bytes);
-            m.setEntity(requestEntity);
-
-            response = client.execute(m, localContext);
-            int status = response.getStatusLine().getStatusCode();
-            if (status >= 200 && status < 300) {
-                // all good
-            } else {
-                throw new RuntimeException("Unexpected response status: " + status);
-            }
-
-        } catch (URISyntaxException | IOException ex) {
-            throw new RuntimeException("server=" + server + "; port=" + port + "; path=" + path, ex);
-        } finally {
-            IOUtils.closeQuietly(response);
-            IOUtils.closeQuietly(client);
-        }
+        return "HttpBlobStore: " + httpTransport.toString();
     }
 }
---
+++ src/main/java/org/hashsplit4j/store/HttpHashStore.java
@@ -0,0 +1,91 @@
+package org.hashsplit4j.store;
+
+import io.milton.common.Path;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.List;
+import org.hashsplit4j.api.Fanout;
+import org.hashsplit4j.api.FanoutSerializationUtils;
+import org.hashsplit4j.api.HashStore;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author brad
+ */
+public class HttpHashStore implements HashStore {
+
+    private static final org.slf4j.Logger log = LoggerFactory.getLogger(HttpBlobStore.class);
+
+    private final HashsplitHttpTransport httpTransport;
+    private final Path fileFanoutPath;
+    private final Path chunkFanoutPath;
+    private long gets;
+    private long sets;
+
+
+    public HttpHashStore(HashsplitHttpTransport httpTransport, String fileFanoutPath, String chunkFanoutPath) {
+        this.httpTransport = httpTransport;
+        this.fileFanoutPath = Path.path(fileFanoutPath);
+        this.chunkFanoutPath = Path.path(chunkFanoutPath);
+    }
+
+    public HttpHashStore(HashsplitHttpTransport httpTransport) {
+        this.httpTransport = httpTransport;
+        this.fileFanoutPath = Path.path("/_hashes/fileFanouts");
+        this.chunkFanoutPath = Path.path("/_hashes/chunkFanouts");
+    }
+
+    @Override
+    public void setChunkFanout(String hash, List<String> blobHashes, long actualContentLength) {
+        throw new UnsupportedOperationException("Cannot write to HTTP hash store");
+    }
+
+    @Override
+    public void setFileFanout(String hash, List<String> fanoutHashes, long actualContentLength) {
+        throw new UnsupportedOperationException("Cannot write to HTTP hash store");
+    }
+
+    @Override
+    public Fanout getFileFanout(String fileHash) {
+        Path destPath = fileFanoutPath.child(fileHash);
+        byte[] arr = httpTransport.get( destPath.toString());
+        if( arr == null ) {
+            log.warn("fanour not found {}", destPath);
+            return null;
+        }
+        ByteArrayInputStream bin = new ByteArrayInputStream(arr);
+        Fanout f;
+        try {
+            f = FanoutSerializationUtils.readFanout(bin);
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+        return f;
+    }
+
+    @Override
+    public Fanout getChunkFanout(String fanoutHash) {
+        Path destPath = chunkFanoutPath.child(fanoutHash);
+        byte[] arr = httpTransport.get(destPath.toString());
+        ByteArrayInputStream bin = new ByteArrayInputStream(arr);
+        Fanout f;
+        try {
+            f = FanoutSerializationUtils.readFanout(bin);
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+        return f;
+    }
+
+    @Override
+    public boolean hasChunk(String fanoutHash) {
+        return getChunkFanout(fanoutHash) != null;
+    }
+
+    @Override
+    public boolean hasFile(String fileHash) {
+        return getFileFanout(fileHash) != null;
+    }
+
+}
