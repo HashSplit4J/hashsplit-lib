0564bc48d55349e4fac677b7e36bb2d5d9393138
Wesley Tuzza <wesley@tuzza.co>
2017-11-21 10:16:07 +1300

    - Update to use BouncyCastle which increases the speed of the Parser - Add support for more SHA algorithms e.g. SHA-1 (Default), SHA-256, SHA-384, SHA-512

--- src/main/java/org/hashsplit4j/api/Crypt.java
+++ src/main/java/org/hashsplit4j/api/Crypt.java
@@ -16,87 +16,97 @@
  */
 package org.hashsplit4j.api;
 
-import java.security.MessageDigest;
 import java.util.List;
+import org.bouncycastle.crypto.Digest;
 
 public class Crypt {
 
     /**
      * Calculates the directory hash of the given string of contents
-     * 
+     *
      * @param content
      * @return
      */
     public static String toHexFromText(String text) {
-        if (text == null)
+        if (text == null) {
             return null;
+        }
 
         return toHexFromByte(text.getBytes());
     }
 
     /**
      * Calculates the directory hash of the given bytes of contents
-     * 
+     *
      * @param content
      * @return
      */
     public static String toHexFromByte(byte[] bytes) {
-        if (bytes == null)
+        if (bytes == null) {
             return null;
+        }
 
-        MessageDigest crypto = Parser.getCrypt();
-        crypto.update(bytes);
+        Digest crypto = Parser.getCrypt();
+        for (byte b : bytes) {
+            crypto.update(b);
+        }
         return Parser.toHex(crypto);
     }
 
     /**
      * Calculates the hash of the given childrens (ie the directory hash with
      * the given childrens)
-     * 
+     *
      * @param children
      * @return
      */
     public static String toHexFromBlob(List<HashGroup> childrens) {
-        MessageDigest crypto = Parser.getCrypt();
+        Digest crypto = Parser.getCrypt();
         for (HashGroup child : childrens) {
             String line = toHashableText(child);
-            crypto.update(line.getBytes());
+
+            for (byte b : line.getBytes()) {
+                crypto.update(b);
+            }
         }
         return Parser.toHex(crypto);
     }
-    
+
     /**
-     * Calculates the hash for the given hashes of Blobs (ie the directory hash with
-     * the given blobs)
-     * 
+     * Calculates the hash for the given hashes of Blobs (ie the directory hash
+     * with the given blobs)
+     *
      * @param childrens
      * @return
      */
     public static String toHexFromHash(List<String> childrens) {
-    	MessageDigest crypto = Parser.getCrypt();
-    	for (String children : childrens) {
-    		String line = children;
-    		line += "\n";
-    		crypto.update(line.getBytes());
-    	}
-    	return Parser.toHex(crypto);
+        Digest crypto = Parser.getCrypt();
+        for (String children : childrens) {
+            String line = children;
+            line += "\n";
+
+            for (byte b : line.getBytes()) {
+                crypto.update(b);
+            }
+        }
+        return Parser.toHex(crypto);
     }
 
     /**
      * Calculates the hash for the given Sub Group for a Root group
-     * 
-     * E.g: Sub Group	
-     * 			{sub group's name} : {hash}
-     * 			{012abc}:{7359c2e2759dae4b07d99643400a838eaa2e7a25}
-     * 		-> hash = SHA1(012abc:7359c2e2759dae4b07d99643400a838eaa2e7a25 + '\n')
-     * 
+     *
+     * E.g: Sub Group {sub group's name} : {hash}
+     * {012abc}:{7359c2e2759dae4b07d99643400a838eaa2e7a25} -> hash =
+     * SHA1(012abc:7359c2e2759dae4b07d99643400a838eaa2e7a25 + '\n')
+     *
      * @param child
      * @return
      */
     private static String toHashableText(HashGroup child) {
         StringBuilder builder = new StringBuilder();
-        if (child == null)
+        if (child == null) {
             return null;
+        }
 
         builder.append(child.getName());
         builder.append(":");
